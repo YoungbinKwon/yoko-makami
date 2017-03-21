38892f5a504cc5524c68efbefbdfb03edf8ae408
keisei.matsumoto <keisei.matsumoto@rakuten.com>
2017-03-21 10:59:18 +0900

    mod file exist bug

--- index.php
+++ index.php
@@ -7,7 +7,9 @@ $incPath = implode(PATH_SEPARATOR, $includes);
 set_include_path(get_include_path() . PATH_SEPARATOR . $incPath);
 
 function __autoload($className){
-    require_once $className . ".php";
+    if (stream_resolve_include_path($className . ".php")) {
+        require_once $className . ".php";
+    }
 }
 
 $dispatcher = new Dispatcher();
