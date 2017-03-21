8616a7d39bc53a2991dd12c459cf3c51a8d5431e
keisei.matsumoto <keisei.matsumoto@rakuten.com>
2017-03-21 09:29:59 +0900

    for debug

--- index.php
+++ index.php
@@ -6,6 +6,10 @@ $includes = array(ROOT_PATH . '/Lib/db/db.php', ROOT_PATH . '/Lib/util', ROOT_PA
 $incPath = implode(PATH_SEPARATOR, $includes);
 set_include_path(get_include_path() . PATH_SEPARATOR . $incPath);
 
+var_dump(ROOT_PATH);
+var_dump(scandir(ROOT_PATH));
+exit();
+
 function __autoload($className){
     require_once $className . ".php";
 }
