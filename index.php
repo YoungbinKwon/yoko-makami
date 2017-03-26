1653dd611248e0c79d16d64648a156d4531d8a1e
Kwon <youngbin.kwon@rakuten.com>
2017-03-27 08:47:41 +0900

    index.php modified

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 define('ROOT_PATH', realpath(dirname(__FILE__)));
 require_once('env_var.php');
 
-$includes = array(ROOT_PATH . '/Lib/db', ROOT_PATH . '/Lib/util', ROOT_PATH . '/Lib/mvc', ROOT_PATH . '/Model');
+$includes = array(ROOT_PATH . '/Lib/db', ROOT_PATH . '/Lib/util', ROOT_PATH . '/Lib/mvc', ROOT_PATH . '/Lib/igo-php-0.1.7/lib', ROOT_PATH . '/Model');
 $incPath = implode(PATH_SEPARATOR, $includes);
 set_include_path(get_include_path() . PATH_SEPARATOR . $incPath);
 
